303c6ba91e2cb90bc7d0457bb55045e8ccaeb895
Edi Permadi <edipermadi@gmail.com>
2017-08-13 13:17:43 +0800

    fixed aes-ecb
    Signed-off-by: Edi Permadi <edipermadi@gmail.com>

--- firmware/teensy-hsm/buffer.h
+++ firmware/teensy-hsm/buffer.h
@@ -16,8 +16,14 @@ typedef struct
     uint8_t data[THSM_DATA_BUF_SIZE];
 } THSM_BUFFER;
 
-typedef struct
+typedef struct buffer_t
 {
+public:
+    buffer_t(uint8_t *buffer, uint32_t buffer_length)
+    {
+        bytes = buffer;
+        length = buffer_length;
+    }
     uint8_t *bytes;
     uint32_t length;
 } buffer_t;
